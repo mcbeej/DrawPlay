e79639523e0c1e785513c1aec78f89df5ca82a6f
mcbeej <mcbee@mymail.mines.edu>
2018-02-26 00:28:50 -0500

    Changed back frame size and color of cat, again, no partner but will include in author tags

--- src/Cat.java
+++ src/Cat.java
@@ -27,7 +27,7 @@ public class Cat {
 		int x=catX;
 		int y=catY;
 		// Draw the head
-		g2.setColor(Color.black);
+		g2.setColor(Color.cyan);
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
 		// Draw the eyes
 		g2.setColor(Color.green);

3c5abda319dd4145a5735d9b7fd251087e8e01b6
mcbeej <mcbee@mymail.mines.edu>
2018-02-26 00:23:56 -0500

    Well, my partner never showed up and didn't connect to the repo on github, so I don't know what to say. Changed the drawing graphics to be more centered.

--- src/MyDrawing.java
+++ src/MyDrawing.java
@@ -14,7 +14,7 @@ public class MyDrawing extends JPanel {
 	// paintComponent is called automatically when the frame needs
 	// to display (e.g., when the program starts)
 	public void paintComponent(Graphics g) {
-		cat.draw(g, 50, 50);
+		cat.draw(g, 100, 100);
 	}
 	
 	/**
